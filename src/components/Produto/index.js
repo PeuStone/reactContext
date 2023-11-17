53964f28ce39fff4ff2b1766494147e07bab9171
PeuStone <pedrohmotaaa@gmail.com>
2023-11-17 09:29:18 -0300

    Criando hook customizado e externalizando função

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,9 +1,9 @@
 import { Container } from './styles';
-import { memo, useContext } from 'react';
+import { memo } from 'react';
 import { IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-import { CarrinhoContext } from 'common/context/Carrinho';
+import { useCarrinhoContext } from 'common/context/Carrinho';
 
 
 function Produto({
@@ -14,21 +14,8 @@ function Produto({
   unidade
 }) {
 
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
-  function adicionarProduto(novoProduto) {
-    const temOProduto = carrinho.some(itemCarrinho => itemCarrinho.id === novoProduto.id);
-
-    if (!temOProduto) {
-      novoProduto.quantidade = 1;
-      return setCarrinho(carrinhoAnterior =>
-        [...carrinhoAnterior, novoProduto])
-    }
-    setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemCarrinho => {
-      if (itemCarrinho.id === novoProduto.id) itemCarrinho.quantidade += 1;
-      return itemCarrinho
-    }))
-
-  }
+  const { carrinho, adicionarProduto } = useCarrinhoContext();
+  const produtoCarrinho = carrinho.find(itemCarrinho => itemCarrinho.id === id)
 
   return (
     <Container>
@@ -47,6 +34,7 @@ function Produto({
         >
           <RemoveIcon />
         </IconButton>
+        {produtoCarrinho?.quantidade || 0}
         <IconButton onClick={() => adicionarProduto({ nome, foto, id, valor })}>
           <AddIcon />
         </IconButton>
