4f1ebbc607fc086ef456f5f541903ddad0ae748e
PeuStone <pedrohmotaaa@gmail.com>
2023-11-20 19:18:56 -0300

    Ajustes na pagina de Login e na pagina de Produto

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -32,6 +32,7 @@ function Produto({
         <IconButton
           color="secondary"
           onClick={() => removerProduto(id)}
+          disabled={!produtoCarrinho}
         >
           <RemoveIcon />
         </IconButton>
